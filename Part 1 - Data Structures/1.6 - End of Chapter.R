82736d196c32b44def4f1a597649ffb55f678677
Ahmet Can GÜNAY <1800002666@stu.iku.edu.tr>
2023-02-13 00:14:23 +0300

    Create 1.6 - End of Chapter.R

---
+++ Part 1 - Data Structures/1.6 - End of Chapter.R
@@ -0,0 +1,14 @@
+# Vectors
+a <- c(1, 2, 3, 4)
+
+# Lists
+l <- list(1, 2, 3)
+l1 <- list("a" = 1, "b" = 3)
+l2 <- list(10, 20, 25, c(8, 10, 12))
+
+# Data Frames
+df <- data.frame("even_nums" = c(2, 4, 6, 8, 10), "odd_nums" = c(1, 3, 5, 7, 9))
+View(df)
+
+# Matrices
+m <- matrix(c(10, 20, 30, 40), nrow = 2, ncol = 2, byrow = TRUE)
