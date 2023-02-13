49d706ecc02a0e1b6e87f65e863f9d10a821e140
Ahmet Can GÜNAY <1800002666@stu.iku.edu.tr>
2023-02-13 22:24:02 +0300

    Create 2.1 - Element Selection in Vectors.R

---
+++ Part 2 - Element Selection from Data Structures/2.1 - Element Selection in Vectors.R
@@ -0,0 +1,20 @@
+x <- c("A", "B", "C")
+
+# The index of the element you want to get is written.(Selecting the first element)
+# Indices start from 1.
+x[1]
+
+# Selecting the second element
+x[2]
+
+y <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Selecting elements in the first and third indexes (including 1 and 3)
+y[1:3]
+
+# Selecting elements which you want to take
+t <- c(25, 41, 35, 86, 14, 62, 1, 46, 32, 1, 54, 2116, 1, 21, 4)
+t[c(3, 5, 7, 9)]
+
+# t[c(1,3,35)]
+# 25 35 NA
